264175191af373fa9a4627ff6e9d842445367f50
gcluelabs <gcluelabs@gclue.jp>
2013-05-25 15:55:27 +0900

    コメント削除

--- src/com/example/messagesample001/MessageSample001Activity.java
+++ src/com/example/messagesample001/MessageSample001Activity.java
@@ -13,26 +13,18 @@ public class MessageSample001Activity extends Activity {
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_message_sample001 );
 
-		// LogCat�ŕ\������
 		String message_log = "TEST LOG";
 		Log.i( "TEST", message_log );
-		// Toast�ŕ\������
 		String message_toast = "TEST TOAST";
 		Toast.makeText(this, message_toast, Toast.LENGTH_LONG).show();
-		// Dialog�ŕ\������
 		String message_dialog = "TEST DIALOG";
 		showDialog( message_dialog );
 	}
 
-	/**
-	 * �_�C�A���O�ŕ������\������B
-	 * @param message �\��������������
-	 */
 	private void showDialog( String message ) {
 		new AlertDialog.Builder( this )
 			.setMessage( message )
 			.setNeutralButton( "YES", new DialogInterface.OnClickListener() {
-				// ���̒���"YES"���̏����������
 				public void onClick( DialogInterface dialog, int whichButton ) {
 				}
 			} ).show();
